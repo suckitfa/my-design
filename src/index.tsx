7e942f4b293bb2413f4847244214b6b6a7ecc5bd
Bob Tang <254202042@qq.com>
2022-11-14 21:02:14 +0800

    styles scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-11-10 21:19:10
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-11-14 21:01:37
+ * @FilePath: /my-design/src/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
